22d539c1d0d5dfa4694976330e79b55cd25b6dd0
Kelly <kelly-oyy@hotmail.com>
2024-09-06 00:20:08 +0800

    revised

--- app/src/main/java/com/example/midtermpractice/MainActivity.kt
+++ app/src/main/java/com/example/midtermpractice/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -91,19 +93,22 @@ fun eligibleForm(navController: NavHostController, modifier: Modifier = Modifier
         TextField(
             value = ageInput,
             onValueChange = {ageInput = it},
-            label = { Text(text = "Age")}
+            label = { Text(text = "Age")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         TextField(
             value = weightInput,
             onValueChange = {weightInput = it},
-            label = { Text(text = "Weight (kg)")}
+            label = { Text(text = "Weight (kg)")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         TextField(
             value = sleepHourInput,
             onValueChange = {sleepHourInput = it},
-            label = { Text(text = "Sleep duration (hr)")}
+            label = { Text(text = "Sleep duration (hr)")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         TextField(
@@ -112,11 +117,13 @@ fun eligibleForm(navController: NavHostController, modifier: Modifier = Modifier
             label = { Text(text = "Health status (Healthy/Sick)")}
         )
 
-        Button(onClick = { navController.navigate("ResultScreen/$result") }) {
+        Button(onClick = { navController.navigate("ResultScreen/$result") },
+            enabled = ageInput.isNotEmpty()&&weightInput.isNotEmpty()&&sleepHourInput.isNotEmpty()&&healthCondition.isNotEmpty()
+        ) {
             Text(text = "Submit")
         }
 
-        Text(text = age.toString() )
+        //Text(text = age.toString() )
     }
 
 
